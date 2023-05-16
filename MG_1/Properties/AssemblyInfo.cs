23d8da3054929d41e928a2a7a5b4b389573929f4
Matt Gramley <matt.gramley@hsn.net>
2023-05-16 14:26:48 -0400

    Add project files.

---
+++ MG_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MG_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("MG_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("66423EC4-A86D-48F9-93F1-9572774D8B4D")]
+[assembly: AssemblyVersion("1.0.0.0")]
